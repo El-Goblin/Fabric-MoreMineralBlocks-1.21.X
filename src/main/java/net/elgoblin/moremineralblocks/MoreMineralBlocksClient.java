2387da064a5c1048b0be6f830b32286a308e186c
El-Goblin <moriyashrinemiko@gmail.com>
2025-05-08 01:22:04 -0300

    Initial Commit

---
+++ src/main/java/net/elgoblin/moremineralblocks/MoreMineralBlocksClient.java
@@ -0,0 +1,10 @@
+package net.elgoblin.moremineralblocks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreMineralBlocksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
