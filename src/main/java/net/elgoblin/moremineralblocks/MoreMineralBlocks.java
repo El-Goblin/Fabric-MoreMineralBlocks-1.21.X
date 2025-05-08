f5df5f7a9023d50b37ebf19007f34c649abe054c
El-Goblin <moriyashrinemiko@gmail.com>
2025-05-08 02:38:05 -0300

    Added pink garnet and raw pink garnet as item and blocks

--- src/main/java/net/elgoblin/moremineralblocks/MoreMineralBlocks.java
+++ src/main/java/net/elgoblin/moremineralblocks/MoreMineralBlocks.java
@@ -1,5 +1,7 @@
 package net.elgoblin.moremineralblocks;
 
+import net.elgoblin.moremineralblocks.block.ModBlocks;
+import net.elgoblin.moremineralblocks.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -12,6 +14,7 @@ public class MoreMineralBlocks implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/elgoblin/moremineralblocks/item/ModItems.java
@@ -0,0 +1,29 @@
+package net.elgoblin.moremineralblocks.item;
+
+import net.elgoblin.moremineralblocks.MoreMineralBlocks;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(MoreMineralBlocks.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        MoreMineralBlocks.LOGGER.info("Registering Mod Items for " + MoreMineralBlocks.MOD_ID);
+
+        // Esto lo pone en la tab de ingredients en creativo
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+            entries.add(RAW_PINK_GARNET);
+        });
+    }
+}
