d99af0788b4472e14cd73ee961137ef67d484d57
Calabrother <111404127+Calabrother@users.noreply.github.com>
2025-08-19 01:01:20 -0300

    Devilmon + Snail Assets Update
    Fixed Devilmon direction
    Fixed Snail direction
    Fixed Devilmon texture (z fighting in wings)
    Added Devilmon idle animation
    Updated Devilmon fly animation
    Added Snail walk animation

--- Assets/Models/Devilmon/Devilmon.java
+++ Assets/Models/Devilmon/Devilmon.java
@@ -6,42 +6,47 @@ public class Devilmon extends EntityModel<Entity> {
 	private final ModelPart Body;
 	private final ModelPart Horns;
 	private final ModelPart Wings;
-	private final ModelPart WingL;
 	private final ModelPart WingR;
+	private final ModelPart WingL;
 	public Devilmon(ModelPart root) {
 		this.Root = root.getChild("Root");
 		this.Body = this.Root.getChild("Body");
 		this.Horns = this.Body.getChild("Horns");
 		this.Wings = this.Root.getChild("Wings");
-		this.WingL = this.Wings.getChild("WingL");
 		this.WingR = this.Wings.getChild("WingR");
+		this.WingL = this.Wings.getChild("WingL");
 	}
 	public static TexturedModelData getTexturedModelData() {
 		ModelData modelData = new ModelData();
 		ModelPartData modelPartData = modelData.getRoot();
 		ModelPartData Root = modelPartData.addChild("Root", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
 
-		ModelPartData Body = Root.addChild("Body", ModelPartBuilder.create().uv(32, 32).cuboid(-1.0F, -3.8F, -1.0F, 2.0F, 2.0F, 2.0F, new Dilation(0.0F))
-		.uv(0, 0).cuboid(-5.0F, -0.3F, -5.0F, 10.0F, 4.0F, 10.0F, new Dilation(0.0F))
-		.uv(0, 25).cuboid(-4.0F, 3.7F, -4.0F, 8.0F, 1.0F, 8.0F, new Dilation(0.0F))
-		.uv(0, 14).cuboid(-4.0F, -2.3F, -4.0F, 8.0F, 3.0F, 8.0F, new Dilation(0.0F))
-		.uv(32, 14).cuboid(-3.0F, -3.3F, -3.0F, 6.0F, 2.0F, 6.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -4.7F, 0.0F));
+		ModelPartData Body = Root.addChild("Body", ModelPartBuilder.create(), ModelTransform.pivot(3.0F, -7.5F, -2.75F));
 
-		ModelPartData Horns = Body.addChild("Horns", ModelPartBuilder.create(), ModelTransform.pivot(-3.0F, -2.8F, 2.75F));
+		ModelPartData cube_r1 = Body.addChild("cube_r1", ModelPartBuilder.create().uv(32, 14).cuboid(-3.0F, -5.0F, -2.0F, 6.0F, 2.0F, 6.0F, new Dilation(0.0F))
+		.uv(0, 0).cuboid(-5.0F, -2.0F, -4.0F, 10.0F, 4.0F, 10.0F, new Dilation(0.0F)), ModelTransform.of(-3.0F, 4.5F, 3.75F, -3.1416F, 0.0F, 3.1416F));
 
-		ModelPartData cube_r1 = Horns.addChild("cube_r1", ModelPartBuilder.create().uv(0, 34).cuboid(0.0F, -2.5F, 0.0F, 1.0F, 4.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(6.0F, 0.0F, 0.0F, -0.223F, -0.6888F, 0.3298F));
+		ModelPartData cube_r2 = Body.addChild("cube_r2", ModelPartBuilder.create().uv(0, 14).cuboid(-4.0F, -5.0F, -3.0F, 8.0F, 3.0F, 8.0F, new Dilation(0.0F)), ModelTransform.of(-3.0F, 5.5F, 3.75F, -3.1416F, 0.0F, 3.1416F));
 
-		ModelPartData cube_r2 = Horns.addChild("cube_r2", ModelPartBuilder.create().uv(4, 34).cuboid(0.0F, -2.5F, 0.0F, 1.0F, 4.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, -0.2624F, -0.7184F, -0.0121F));
+		ModelPartData cube_r3 = Body.addChild("cube_r3", ModelPartBuilder.create().uv(0, 25).cuboid(-4.0F, 0.0F, -3.0F, 8.0F, 1.0F, 8.0F, new Dilation(0.0F)), ModelTransform.of(-3.0F, 6.5F, 3.75F, -3.1416F, 0.0F, 3.1416F));
 
-		ModelPartData Wings = Root.addChild("Wings", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+		ModelPartData cube_r4 = Body.addChild("cube_r4", ModelPartBuilder.create().uv(32, 32).cuboid(-1.0F, -5.0F, 0.0F, 2.0F, 2.0F, 2.0F, new Dilation(0.0F)), ModelTransform.of(-3.0F, 4.0F, 3.75F, -3.1416F, 0.0F, 3.1416F));
 
-		ModelPartData WingL = Wings.addChild("WingL", ModelPartBuilder.create(), ModelTransform.pivot(-4.0F, -3.0F, -1.0F));
+		ModelPartData Horns = Body.addChild("Horns", ModelPartBuilder.create(), ModelTransform.of(0.0F, 0.0F, 0.0F, -3.1416F, 0.0F, 3.1416F));
 
-		ModelPartData cube_r3 = WingL.addChild("cube_r3", ModelPartBuilder.create().uv(32, 22).cuboid(-10.0F, -5.0F, 0.0F, 10.0F, 5.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.2618F));
+		ModelPartData cube_r5 = Horns.addChild("cube_r5", ModelPartBuilder.create().uv(0, 34).cuboid(0.0F, -2.5F, 0.0F, 1.0F, 4.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(6.0F, 0.0F, 0.0F, -0.223F, -0.6888F, 0.3298F));
 
-		ModelPartData WingR = Wings.addChild("WingR", ModelPartBuilder.create(), ModelTransform.pivot(4.0F, -3.0F, -1.0F));
+		ModelPartData cube_r6 = Horns.addChild("cube_r6", ModelPartBuilder.create().uv(4, 34).cuboid(0.0F, -2.5F, 0.0F, 1.0F, 4.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, -0.2624F, -0.7184F, -0.0121F));
 
-		ModelPartData cube_r4 = WingR.addChild("cube_r4", ModelPartBuilder.create().uv(32, 27).cuboid(0.0F, -5.0F, 0.0F, 10.0F, 5.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, -0.2618F));
+		ModelPartData Wings = Root.addChild("Wings", ModelPartBuilder.create(), ModelTransform.pivot(-4.0F, -3.0F, 1.0F));
+
+		ModelPartData WingR = Wings.addChild("WingR", ModelPartBuilder.create(), ModelTransform.of(0.0F, 0.0F, 0.0F, -3.1416F, 0.0F, 3.1416F));
+
+		ModelPartData cube_r7 = WingR.addChild("cube_r7", ModelPartBuilder.create().uv(32, 27).cuboid(0.0F, -5.0F, 0.0F, 10.0F, 5.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, -0.2618F));
+
+		ModelPartData WingL = Wings.addChild("WingL", ModelPartBuilder.create(), ModelTransform.of(8.0F, 0.0F, 0.0F, -3.1416F, 0.0F, 3.1416F));
+
+		ModelPartData cube_r8 = WingL.addChild("cube_r8", ModelPartBuilder.create().uv(32, 22).cuboid(-10.0F, -5.0F, 0.0F, 10.0F, 5.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.2618F));
 		return TexturedModelData.of(modelData, 64, 64);
 	}
 	@Override
--- Assets/Models/Snail/Snail.java
+++ Assets/Models/Snail/Snail.java
@@ -3,39 +3,51 @@
 // Paste this class into your mod and generate all required imports
 public class Snail extends EntityModel<Entity> {
 	private final ModelPart Root;
-	private final ModelPart Body;
 	private final ModelPart Shell;
+	private final ModelPart Body;
+	private final ModelPart Main;
+	private final ModelPart Tail;
 	private final ModelPart Antenae;
 	private final ModelPart AntenaeL;
 	private final ModelPart AntenaeR;
 	public Snail(ModelPart root) {
 		this.Root = root.getChild("Root");
-		this.Body = this.Root.getChild("Body");
 		this.Shell = this.Root.getChild("Shell");
-		this.Antenae = this.Root.getChild("Antenae");
+		this.Body = this.Root.getChild("Body");
+		this.Main = this.Body.getChild("Main");
+		this.Tail = this.Body.getChild("Tail");
+		this.Antenae = this.Body.getChild("Antenae");
 		this.AntenaeL = this.Antenae.getChild("AntenaeL");
 		this.AntenaeR = this.Antenae.getChild("AntenaeR");
 	}
 	public static TexturedModelData getTexturedModelData() {
 		ModelData modelData = new ModelData();
 		ModelPartData modelPartData = modelData.getRoot();
-		ModelPartData Root = modelPartData.addChild("Root", ModelPartBuilder.create(), ModelTransform.pivot(-1.0F, 24.0F, 0.0F));
+		ModelPartData Root = modelPartData.addChild("Root", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
 
-		ModelPartData Body = Root.addChild("Body", ModelPartBuilder.create().uv(0, 0).cuboid(0.0F, -2.0F, -2.0F, 2.0F, 2.0F, 6.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+		ModelPartData Shell = Root.addChild("Shell", ModelPartBuilder.create(), ModelTransform.pivot(0.5F, -0.75F, 1.25F));
 
-		ModelPartData cube_r1 = Body.addChild("cube_r1", ModelPartBuilder.create().uv(14, 8).cuboid(-1.0F, -0.5F, -3.0F, 2.0F, 1.0F, 3.0F, new Dilation(0.0F)), ModelTransform.of(1.0F, -0.5F, -1.75F, 0.2182F, 0.0F, 0.0F));
+		ModelPartData cube_r1 = Shell.addChild("cube_r1", ModelPartBuilder.create().uv(0, 8).cuboid(-1.0F, -4.1941F, -1.2756F, 3.0F, 4.0F, 4.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, 2.8798F, 0.0F, -3.1416F));
 
-		ModelPartData Shell = Root.addChild("Shell", ModelPartBuilder.create().uv(0, 8).cuboid(-1.0F, -4.1941F, -1.2756F, 3.0F, 4.0F, 4.0F, new Dilation(0.0F)), ModelTransform.of(0.5F, -0.75F, -1.25F, -0.2618F, 0.0F, 0.0F));
+		ModelPartData Body = Root.addChild("Body", ModelPartBuilder.create(), ModelTransform.pivot(1.0F, 0.0F, -1.0F));
 
-		ModelPartData Antenae = Root.addChild("Antenae", ModelPartBuilder.create(), ModelTransform.pivot(0.5F, -2.5F, 3.0F));
+		ModelPartData Main = Body.addChild("Main", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
-		ModelPartData AntenaeL = Antenae.addChild("AntenaeL", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 0.0F, -1.0F));
+		ModelPartData cube_r2 = Main.addChild("cube_r2", ModelPartBuilder.create().uv(0, 0).cuboid(0.0F, -2.0F, -3.0F, 2.0F, 2.0F, 6.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, -3.1416F, 0.0F, 3.1416F));
 
-		ModelPartData cube_r2 = AntenaeL.addChild("cube_r2", ModelPartBuilder.create().uv(14, 12).cuboid(-0.5F, -2.0F, 0.0F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 1.0F, -0.7681F, -0.1841F, -0.1872F));
+		ModelPartData Tail = Body.addChild("Tail", ModelPartBuilder.create(), ModelTransform.pivot(-1.0F, -0.5F, 2.75F));
 
-		ModelPartData AntenaeR = Antenae.addChild("AntenaeR", ModelPartBuilder.create(), ModelTransform.pivot(1.0F, 0.0F, -1.0F));
+		ModelPartData cube_r3 = Tail.addChild("cube_r3", ModelPartBuilder.create().uv(14, 8).cuboid(-1.0F, -0.5F, -3.0F, 2.0F, 1.0F, 3.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, -2.9234F, 0.0F, 3.1416F));
 
-		ModelPartData cube_r3 = AntenaeR.addChild("cube_r3", ModelPartBuilder.create().uv(14, 15).cuboid(-0.5F, -2.0F, 0.0F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 1.0F, -0.7681F, 0.1841F, 0.1872F));
+		ModelPartData Antenae = Body.addChild("Antenae", ModelPartBuilder.create(), ModelTransform.pivot(-1.0F, -2.0F, -3.0F));
+
+		ModelPartData AntenaeL = Antenae.addChild("AntenaeL", ModelPartBuilder.create(), ModelTransform.of(0.7588F, -0.8295F, -0.0366F, -3.1416F, 0.0F, 3.1416F));
+
+		ModelPartData cube_r4 = AntenaeL.addChild("cube_r4", ModelPartBuilder.create().uv(14, 12).cuboid(-0.5F, -1.0F, -0.5F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, -0.7681F, -0.1841F, -0.1872F));
+
+		ModelPartData AntenaeR = Antenae.addChild("AntenaeR", ModelPartBuilder.create(), ModelTransform.of(-0.7588F, -0.8295F, -0.0366F, -3.1416F, 0.0F, 3.1416F));
+
+		ModelPartData cube_r5 = AntenaeR.addChild("cube_r5", ModelPartBuilder.create().uv(14, 15).cuboid(-0.5F, -1.0F, -0.5F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, -0.7681F, 0.1841F, 0.1872F));
 		return TexturedModelData.of(modelData, 32, 32);
 	}
 	@Override
