cf21370b2fd638c6fa2050fc161a964cb56a7184
Calabrother <111404127+Calabrother@users.noreply.github.com>
2026-07-28 19:27:32 -0300

    Updated Dimensional Pocket Model

--- Assets/Models/DimensionalPocket/Dimensional Pocket.java
+++ Assets/Models/DimensionalPocket/Dimensional Pocket.java
@@ -1,4 +1,4 @@
-// Made with Blockbench 5.1.4
+// Made with Blockbench 5.1.6
 // Exported for Minecraft version 1.17+ for Yarn
 // Paste this class into your mod and generate all required imports
 public class Dimensional Pocket extends EntityModel<Entity> {
@@ -15,7 +15,8 @@ public class Dimensional Pocket extends EntityModel<Entity> {
 		ModelPartData modelPartData = modelData.getRoot();
 		ModelPartData Hips = modelPartData.addChild("Hips", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
 
-		ModelPartData Bottom = Hips.addChild("Bottom", ModelPartBuilder.create().uv(0, 28).cuboid(-7.0F, -3.0F, -7.0F, 14.0F, 3.0F, 14.0F, new Dilation(0.0F))
+		ModelPartData Bottom = Hips.addChild("Bottom", ModelPartBuilder.create().uv(40, 46).cuboid(-5.0F, -3.0F, 6.0F, 10.0F, 9.0F, 2.0F, new Dilation(0.0F))
+		.uv(0, 28).cuboid(-7.0F, -3.0F, -7.0F, 14.0F, 3.0F, 14.0F, new Dilation(0.0F))
 		.uv(0, 45).cuboid(-5.0F, -3.0F, -5.0F, 10.0F, 2.0F, 10.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
 		ModelPartData Top = Hips.addChild("Top", ModelPartBuilder.create().uv(0, 0).cuboid(-7.0F, -3.0F, -14.0F, 14.0F, 3.0F, 14.0F, new Dilation(0.0F))
